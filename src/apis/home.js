d43db4a6cb6fac39a748e50d4ef581fc85a3976b
daozaixi <3256949278@qq.com>
2024-10-14 00:50:46 +0800

    面板组件封装，新鲜好物和人气推荐设计

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,7 +1,21 @@
 // 获取首页轮播图数据
 import httpInstance from '@/utils/http'
-export function getBannerAPI() {
+export const getBannerAPI = () => {
     return httpInstance({
         url: 'home/banner'
+    })
+}
+
+// 获取新鲜好物
+export const findNewAPI = () => {
+    return httpInstance({
+        url: 'home/new'
+    })
+}
+
+// 获取人气推荐
+export const getHotAPI = () => {
+    return httpInstance({
+        url: 'home/hot'
     })
 }
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from "./components/Homepanel.vue"
 </script>
 
 <template>
@@ -13,5 +14,18 @@ import homeProduct from './components/HomeProduct.vue'
     </div>
     <HomeNew />
     <HomeHot />
-    <homeProduct />
+    <HomeProduct />
+
+    <!-- 测试面板组件抽象 -->
+    <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+        <div>
+            woshi一个新鲜好物的插槽
+        </div>
+    </HomePanel>
+
+    <HomePanel title="人气推荐" sub-title="人气推荐 爆款商品">
+        <div>
+            woshi一个人气推荐的插槽
+        </div>
+    </HomePanel>
 </template>
