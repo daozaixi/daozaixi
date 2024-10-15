a8f0bb83736578bfae6406ef988a402e857fbd29
daozaixi <3256949278@qq.com>
2024-10-15 17:59:01 +0800

    二级商品渲染

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,5 +1,5 @@
 import request from '@/utils/http'
-import httpInstance from '@/utils/http'
+// 获取一级分类
 export function getCategoryAPI(id) {
     return request({
         url: '/category',
@@ -10,11 +10,21 @@ export function getCategoryAPI(id) {
     })
 }
 
+// 二级面包屑
 export const getCategoryFilterAPI = (id) => {
     return request({
         url: '/category/sub/filter',
         params: {
             id
         }
+    })
+}
+
+// 二级分类商品渲染
+export const getSubCategoryAPI = (data) => {
+    return request({
+        url: '/category/goods/temporary',
+        method: 'POST',
+        data
     })
 }
