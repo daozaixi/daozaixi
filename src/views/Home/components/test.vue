2784d8cea02e91a051b2362dd68df1a8589f15e0
daozaixi <3256949278@qq.com>
2024-11-01 12:02:05 +0800

    test

---
+++ src/views/Home/components/test.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+const music = ref({
+    title: 'The Dark Side of the Moon',
+    artist: 'Pink Floyd',
+    album: 'The Dark Side of the Moon (Special Edition)',
+    year: '1973'
+})
+</script>
+
+<template>
+    <ul>
+        <template v-for="item in music">
+            <li>{{ item }}</li>
+        </template>
+    </ul>
+</template>
+
+<style scoped></style>
